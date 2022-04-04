a3c2a9e64828cf87e96dba00756c804d4c7c3a51
Alberto Centonze <albicento.ac@gmail.com>
2022-04-04 16:47:57 +0200

    Added initial files for week03

---
+++ week03/03_CounterSeller.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+contract BalelecTicketSeller {
+    uint public price = 30;
+    uint public ticketsCounter = 0;
+    address public immutable owner;
+    mapping(address => bool) public tickets;
+    event Purchase(address buyer);
+
+    /*
+    * This error appears when ...
+    * caller - the address of the users that called the function without being authorized...
+    */
+    error Unauthorized(address caller);
+    error WrongAmount(uint amount);
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    function SetPrice(uint newPrice) public onlyOwner {
+        if (newPrice >= 100) {
+            revert WrongAmount(newPrice);
+        }
+        price = newPrice;
+    }
+
+    //custom modifier
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert Unauthorized(msg.sender);
+        }
+        _;
+    }
+
+    function BuyTicket() payable public {
+        if (msg.value != price) {
+            revert WrongAmount(msg.value);
+        }
+        tickets[msg.sender] = true;
+        emit Purchase(msg.sender);
+        ++ticketsCounter;
+    }
+}
---
+++ week03/04_AutonomousSeller.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+contract BalelecTicketSeller {
+    uint public immutable price;
+    uint public immutable priceAfterThreshold;
+    uint public immutable threshold;
+    uint public ticketsCounter = 0; 
+    mapping(address => bool) public tickets;
+    event Purchase(address buyer);
+
+    /*
+    * This error appears when ...
+    * caller - the address of the users that called the function without being authorized...
+    */
+    error Unauthorized(address caller);
+    error WrongAmount(uint amount);
+
+    constructor(
+        uint _threshold,
+        uint _price,
+        uint _priceAfterThreshold)
+    {
+        priceAfterThreshold = _priceAfterThreshold;
+        price = _price;
+        threshold = _threshold;
+    }
+
+
+    function getPrice() public view returns (uint) {
+        return ticketsCounter <= threshold ? price : priceAfterThreshold;
+    }
+
+    function BuyTicket() payable public {
+        if (msg.value != price) {
+            revert WrongAmount(msg.value);
+        }
+        tickets[msg.sender] = true;
+        emit Purchase(msg.sender);
+        ++ticketsCounter;
+    }
+}
